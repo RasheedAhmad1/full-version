99402ef0087d2c504fd6a6bf9a3c580f4b335c77
rasheedahmad1 <rasheedahmad22333@gmail.com>
2024-10-09 16:43:04 -0700

    Factory added

---
+++ Modules/Book/Database/factories/BookFactory.php
@@ -0,0 +1,27 @@
+<?php
+namespace Modules\Book\Database\factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \Modules\Book\Entities\Book::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
+
